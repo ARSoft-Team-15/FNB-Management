8ebc4e8a0c6190a64ca7d2311e22b8d233e3e04f
Naufal Widhi <naufalwidhi1@gmail.com>
2022-05-11 10:33:15 +0700

    add Login Model & Migration

---
+++ app/Models/UserManagement/d_user.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\UserManagement;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class d_user extends Model
+{
+    use HasFactory;
+}
